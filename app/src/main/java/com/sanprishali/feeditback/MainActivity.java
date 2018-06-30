5da88315b65c23ed2c0c6a84fe87621d638816ae
sandipan144 <sandipanchakraborty144@gmail.com>
2018-06-30 17:47:23 +0530

    Checking done

--- app/src/main/java/com/sanprishali/feeditback/MainActivity.java
+++ app/src/main/java/com/sanprishali/feeditback/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //I am Priyanshu
         //Sandipan is Chutiya
+        //Tu bhi Chutiya hi hai
     }
 }
