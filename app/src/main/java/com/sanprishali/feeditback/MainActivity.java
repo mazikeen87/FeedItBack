73a0b861ef24b7556c612c66041248540f8dc98b
priy0396 <priyanshu.saxena0396@gmail.com>
2018-06-30 18:00:54 +0530

    Commit message

--- app/src/main/java/com/sanprishali/feeditback/MainActivity.java
+++ app/src/main/java/com/sanprishali/feeditback/MainActivity.java
@@ -9,8 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //I am Priyanshu
-        //Sandipan is Chutiya
-        //Tu bhi Chutiya hi hai
+        
     }
 }
