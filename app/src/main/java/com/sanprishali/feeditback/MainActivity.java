48733d9436b00ff2a95265e800cd807fcf66ae3d
sandipan144 <sandipanchakraborty144@gmail.com>
2018-07-02 02:59:15 +0530

    Added Layout for login and signup modified

--- app/src/main/java/com/sanprishali/feeditback/MainActivity.java
+++ app/src/main/java/com/sanprishali/feeditback/MainActivity.java
@@ -1,17 +1,28 @@
 package com.sanprishali.feeditback;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
 
     Button buttonLogin,buttonSignup;
     LinearLayout loginLayout,signupLayout;
     TextView loginTextViewHint,signupTextViewHint;
+    Spinner spinnerDesignation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +33,18 @@ public class MainActivity extends AppCompatActivity {
         signupLayout = (LinearLayout)findViewById(R.id.signupLayout);
         signupTextViewHint = (TextView)findViewById(R.id.signupTextViewHint);
         loginTextViewHint = (TextView)findViewById(R.id.loginTextViewHint);
+        spinnerDesignation = (Spinner)findViewById(R.id.spinnerDesgination);
+
+        String[] designation = new String[]{
+                "Designation",
+                "HR",
+                "Food"
+        };
+        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(
+                this,R.layout.spinner_layout,designation
+        );
+        spinnerArrayAdapter.setDropDownViewResource(R.layout.spinner_layout);
+        spinnerDesignation.setAdapter(spinnerArrayAdapter);
 
         signupTextViewHint.setOnClickListener(new View.OnClickListener() {
             @Override
