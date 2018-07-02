4f65503229e39b9958c7afa8a4d70cacd960d5d1
sandipan144 <sandipanchakraborty144@gmail.com>
2018-07-02 17:09:34 +0530

    Added Layout for login and signup modified 1

--- app/src/main/java/com/sanprishali/feeditback/MainActivity.java
+++ app/src/main/java/com/sanprishali/feeditback/MainActivity.java
@@ -1,15 +1,18 @@
 package com.sanprishali.feeditback;
 
 import android.content.Intent;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -22,7 +25,9 @@ public class MainActivity extends AppCompatActivity {
     Button buttonLogin,buttonSignup;
     LinearLayout loginLayout,signupLayout;
     TextView loginTextViewHint,signupTextViewHint;
+    TextInputEditText editTextemail,editTextpassword,editTextname,editTextemail1,editTextpassword1,editTextpassword2;
     Spinner spinnerDesignation;
+    String designation1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,17 +39,42 @@ public class MainActivity extends AppCompatActivity {
         signupTextViewHint = (TextView)findViewById(R.id.signupTextViewHint);
         loginTextViewHint = (TextView)findViewById(R.id.loginTextViewHint);
         spinnerDesignation = (Spinner)findViewById(R.id.spinnerDesgination);
+        editTextemail = (TextInputEditText)findViewById(R.id.editTextEmail);
+        editTextpassword = (TextInputEditText)findViewById(R.id.editTextPassword);
+        editTextname = (TextInputEditText)findViewById(R.id.editTextName);
+        editTextemail1 = (TextInputEditText)findViewById(R.id.editTextEmail1);
+        editTextpassword1 = (TextInputEditText)findViewById(R.id.editTextPassword1);
+        editTextpassword2 = (TextInputEditText)findViewById(R.id.editTextPassword2);
+
+        String email = editTextemail.getText().toString();
+        String password = editTextpassword.getText().toString();
+        String name = editTextname.getText().toString();
+        String email1 = editTextemail1.getText().toString();
+        String password1 = editTextpassword1.getText().toString();
+        String password2 = editTextpassword2.getText().toString();
 
         String[] designation = new String[]{
-                "Designation",
+                "",
                 "HR",
-                "Food"
+                "Food",
+                "Employee"
         };
         ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(
                 this,R.layout.spinner_layout,designation
         );
         spinnerArrayAdapter.setDropDownViewResource(R.layout.spinner_layout);
         spinnerDesignation.setAdapter(spinnerArrayAdapter);
+        spinnerDesignation.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                designation1 = spinnerDesignation.getSelectedItem().toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                //
+            }
+        });
 
         signupTextViewHint.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,15 +94,23 @@ public class MainActivity extends AppCompatActivity {
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //login activity
+                loginActivity();
             }
         });
 
         buttonSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //signup activity
+                signupActivity();
             }
         });
+    }
+
+    public void loginActivity(){
+
+    }
+
+    public void signupActivity(){
+
     }
 }
